b0029eab2ad8470d95ad57b71003c66550468485
maybehari13 <39975818+maybehari13@users.noreply.github.com>
2019-06-09 11:18:16 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { UserComponent } from './user/user.component';
+import { ProjectComponent } from './project/project.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddTaskComponent,
+    ViewTaskComponent,
+    UserComponent,
+    ProjectComponent,
+    WelcomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
